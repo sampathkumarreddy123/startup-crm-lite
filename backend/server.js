4b79a050e754b609bfdb3437569062844c7a9962
sampathkumarreddy123 <sampathkumarreddy044@gmail.com>
2026-07-07 11:11:40 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -126,7 +126,7 @@ app.use("/api/leads", leadRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
   });
 }
